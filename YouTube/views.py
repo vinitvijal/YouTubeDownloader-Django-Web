5ee0000a65e318f88428d186cc61af7234c33280
Vinu <vinu@Vinus-MacBook-Air.local>
2021-12-12 00:35:05 +0530

    verify button working

--- YouTube/views.py
+++ YouTube/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from pytube import YouTube
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method =='POST':
+        name = request.POST.get('link')
+        print(name)
+        yt = YouTube(name)
+        videoName = yt.title
+        print(videoName)
+        context = {
+            'videoName': videoName
+        }
+        return render(request, 'index.html', context)
+    else:
+
+        return render(request, 'index.html')
+
+
+
+
+
