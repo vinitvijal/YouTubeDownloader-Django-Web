9714b183bcd07d082c6dc232a36da2765356fb1e
Vinit Kumar Vijal <vineet.shanker@soesector17rohini.co.in>
2021-12-12 20:04:51 +0530

    Cleaning Used Media Function, Long Video Direct Download

--- YouTube/views.py
+++ YouTube/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from pytube import YouTube
+import os
+import shutil
 
 # Create your views here.
 def index(request):
@@ -8,36 +10,71 @@ def index(request):
         print(name)
         yt = YouTube(name)
         videoName = yt.title
+        length = yt.length
+
+        audioStream = yt.streams.filter(type='audio', abr='128kbps')[0].url
+        print("audio = \n", audioStream, '\n\n')
+        videoStream = yt.streams.filter(progressive='true')[-1].url
+        print("video = \n", videoStream, '\n\n')
+
         fileName = (videoName.replace(' ' ,'_')).replace('|','-')
         print(videoName)
         print()
         thumbnail = yt.thumbnail_url
-        stream = yt.streams.filter(type='audio')
+        stream = yt.streams.filter(type='video')
         for i in stream:
             print(i, end='\n\n')
-        if 'videoMP4' in request.POST:
-            video = yt.streams.get_highest_resolution()
-            location = '/media/videos/' + fileName + '(CodeVinu).mp4'
-            video.download(output_path='media/videos/', filename=(fileName + '(CodeVinu).mp4'))
 
-        elif 'audioMP3' in request.POST:
-            audio = yt.streams.get_audio_only()
-            location = '/media/audio/' + fileName + '(CodeVinu).mp3'
-            audio.download(output_path='media/audio/', filename=(fileName + '(CodeVinu).mp3'))
+        if length <= 1200:
+            if 'videoMP4' in request.POST:
+                clean('videos')
+                video = yt.streams.get_highest_resolution()
+                location = '/media/videos/' + fileName + '(CodeVinu).mp4'
+                video.download(output_path='media/videos/', filename=(fileName + '(CodeVinu).mp4'))
+
+            elif 'audioMP3' in request.POST:
+                clean('audio')
+                audio = yt.streams.get_audio_only()
+                location = '/media/audio/' + fileName + '(CodeVinu).mp3'
+                audio.download(output_path='media/audio/', filename=(fileName + '(CodeVinu).mp3'))
+            else:
+                index(request)
+
+            context = {
+                'videoName': videoName,
+                'location': location,
+                "thumbnail": thumbnail
+            }
+            return render(request, 'index.html', context)
         else:
-            index(request)
 
-        context = {
-            'videoName': videoName,
-            'location': location,
-            "thumbnail": thumbnail
-        }
-        return render(request, 'index.html', context)
+            if 'videoMP4' in request.POST:
+                location = videoStream
+
+            elif 'audioMP3' in request.POST:
+                location = audioStream
+            context = {
+                'Disclamer': "Your Video Length is More Than 20 Min",
+                "videoName": videoName,
+                'location': location,
+                "thumbnail": thumbnail
+            }
+            return render(request, 'index.html', context)
 
     else:
 
         return render(request, 'index.html')
 
 
+def clean(folder='audio'):
+    directory = folder
+    parent_dir = os.path.dirname(os.path.abspath('manage.py'))+'/media'
+    print(parent_dir)
+    # Removing Folders
+    path = os.path.join(parent_dir, directory)
+    shutil.rmtree(path)
+    # Create Folders
+    os.mkdir(path)
 
+    return print("Done")
 
