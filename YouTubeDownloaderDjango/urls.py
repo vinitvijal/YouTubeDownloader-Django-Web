c89d6d4d77f2d8e19115b3d8763f2167be09bb28
Vinit Kumar Vijal <vineet.shanker@soesector17rohini.co.in>
2021-12-16 23:04:24 +0530

    Update urls.py

--- YouTubeDownloaderDjango/urls.py
+++ YouTubeDownloaderDjango/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 from YouTube import views
 from django.conf.urls import url
 from django.conf import settings
 from django.views.static import serve
 from django.contrib.staticfiles.storage import staticfiles_storage
-from django.urls import include, path
 from django.views.generic.base import RedirectView
 from django.conf import settings
 
