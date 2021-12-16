302eae3166afc150f662f848c2c094eeb0aaa123
Vinit Kumar Vijal <vineet.shanker@soesector17rohini.co.in>
2021-12-16 16:04:11 +0530

    Favicon

--- YouTubeDownloaderDjango/urls.py
+++ YouTubeDownloaderDjango/urls.py
@@ -14,14 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 from YouTube import views
 from django.conf.urls import url
 from django.conf import settings
 from django.views.static import serve
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from django.conf import settings
+
 
 
 urlpatterns = [
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
     path('admin/', admin.site.urls),
     path('', views.index),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
